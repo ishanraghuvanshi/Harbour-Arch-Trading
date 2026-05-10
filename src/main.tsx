50c3d4c7bcec988855c835ca9c90de1f7d48f312
ishanraghuvanshi <ishan@harbourarchtrading.com.au>
2026-05-10 22:38:09 +1000

    Test render

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <div style={{ color: "white", padding: "2rem" }}>
+    Test render – if you see this, React is working.
+  </div>
+);
