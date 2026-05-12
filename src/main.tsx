f06a96f57e778f8a41a6dd68ec155a62c0b11117
Ishan R <ishan@harbourarchtrading.com.au>
2026-05-12 18:14:59 +1000

    resolve merge conflicts

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-<<<<<<< HEAD
-import "./packs-section.css"
-=======
->>>>>>> c74a1700dab43502ea6f630788d451a0f8fa8304
+import "./packs-section.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
